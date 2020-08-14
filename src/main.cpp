0f624e482831f75cf6509be74707d7b125629b68
Pessi Päivärinne <pessip@pessi-isilinux.localdomain>
2020-08-14 23:40:57 +0300

    Added JSON

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 // Import Libraries
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 #include <MQTT.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ArduinoJson.h>
 
 // Define Pin locations and temperature precision
 #define ONE_WIRE_BUS 15
 #define TEMPERATURE_PRECISION 9
 
 // Setup instances
-WiFiClient net;                      // WiFi Client
+WiFiClient net; // WiFi Client
+WiFiUDP ntpUDP; // UDP Client for ntp
+NTPClient timeClient(ntpUDP);
 MQTTClient client;                   // MQTT Client
 OneWire oneWire(ONE_WIRE_BUS);       // OneWire Bus
 DallasTemperature sensors(&oneWire); // DallasTemperature and pass oneWire for OneWire comms.
@@ -26,6 +31,7 @@ int postN = 0;
 const char *ssid = "DNA-WLAN-2G-AA38";
 const char *password = "88780760472";
 
+// WiFi connect
 void connect()
 {
   // Check wifi connection
@@ -47,10 +53,36 @@ void connect()
   client.subscribe("/moekki"); // Subscribe to the mökki MQTT topic
 }
 
+// Wifi Info
+void wifiInfo()
+{
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 // Print incoming message to Serial Monitor
 void messageReceived(String &topic, String &payload)
 {
   Serial.println("incoming: " + topic + " - " + payload);
+}
+
+// Sensor Info
+void sensorInfo()
+{
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: ");
+  if (sensors.isParasitePowerMode())
+    Serial.println("ON");
+  else
+    Serial.println("OFF");
 }
 
 void setup()
@@ -73,10 +105,11 @@ void setup()
   }
 
   // Wifi Info
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  wifiInfo();
+
+  // Setup time
+  timeClient.begin();
+  timeClient.setTimeOffset(10800);
 
   // COnnect to MQTT Broker
   client.begin("broker.shiftr.io", net);
@@ -86,22 +119,8 @@ void setup()
   // Start up the library
   sensors.begin();
 
-  // locate devices on the bus
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  Serial.print(sensors.getDeviceCount(), DEC);
-  Serial.println(" devices.");
-
-  // report parasite power requirements
-  Serial.print("Parasite power is: ");
-  if (sensors.isParasitePowerMode())
-    Serial.println("ON");
-  else
-    Serial.println("OFF");
-
-  // // Pin Modes
-  // pinMode(Led, OUTPUT);
-  // pinMode(LED_BUILTIN, OUTPUT);
+  // Sensor Info
+  sensorInfo();
 }
 
 void loop()
@@ -118,15 +137,27 @@ void loop()
   // publish a message counting up roughly every second.
   if (millis() - lastMillis > 1000)
   {
-    Serial.print(" Requesting temperatures...");
     sensors.requestTemperatures(); // Send the command to get temperature readings
-    Serial.println("DONE");
-    /********************************************************************/
-    Serial.print("Temperature is: ");
-    Serial.println(sensors.getTempCByIndex(0));
-    String content = "Tempreature is: " + String(sensors.getTempCByIndex(0));
-    lastMillis = millis();
+
+    // Build JSON
+    // Init JSON
+    const int capacity = JSON_OBJECT_SIZE(3);
+    StaticJsonDocument<capacity> doc;
+
+    // Input Data
+    doc["temperature"] = sensors.getTempCByIndex(0);
+    doc["time"] = timeClient.getEpochTime();
+    doc["postN"] = postN;
+
+    // Serialize JSON
+    String content;
+    serializeJson(doc, content);
+
+    // Publish to MQTT
     client.publish("/moekki", content);
+
+    // Reset millis and increment postN
+    lastMillis = millis();
     ++postN;
   }
 }
